456646586a3316efbec3badf30b06c04ecf6e28e
Maxim Afanasev <maximafanasyevtodo@yandex.ru>
2024-02-29 23:48:42 +0300

    upd

--- 6 theory files.py
+++ 6 theory files.py
@@ -1,19 +1,23 @@
 import os
 print('-----------------')
-#создать ссылку из строк
+# создать ссылку из строк
 print('#1', os.path.join('usr', 'bin', 'spam')) 
-#текущий каталог
+# текущий каталог
 print('#2', os.getcwd()) 
-#изменить путь до каталога
+# изменить путь до каталога
 print('#3', os.chdir('C:\\Windows')) 
 # каталог изменился
 print('#4', os.getcwd()) 
-#создать папку 1 раз
-#print('#5', os.makedirs('C:\\Users\\Swizi\\Desktop\\www'))
+# создать папку 1 раз
+# print('#5', os.makedirs('C:\\Users\\Swizi\\Desktop\\www'))
 print('-----------------')
+# aбсолютный путь
 print('#1', os.path.abspath('.'))
-print('#2', )
-print('#3', )
-print('#4', )
+# перейти в папку Scripts
+print('#3', os.path.abspath('.\\Scripts'))
+# проверка на абсолютный путь
+print('#3', os.path.isabs('.'))
+# возврат строки относительного пути
+print('#4', os.path.relpath('C:\\Windows'))
 print('#5', )
 
